905f3c9b8c378751b523c5f5a071e35231641e39
Eliazar Melendez <eliazar.melendez@gmail.com>
2023-09-13 09:15:59 -0600

    MVVM basics

---
+++ MyAwesomeApp/MyAwesomeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyAwesomeApp.ViewModels
+{
+    public class CoffeeEquipmentViewModel : BindableObject
+    {
+        int count = 0;
+        string countDisplay = "Click Me!";
+
+        public ICommand IncreaseCount { get; }
+
+        public string CountDisplay
+        {
+            get { return countDisplay; }
+
+            set
+            {
+                if (value == countDisplay) return;
+
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+
+        public void OnIncrease()
+        {
+            CountDisplay = $"You clicked {++count} time(s)";
+        }
+    }
+}
--- MyAwesomeApp/MyAwesomeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyAwesomeApp/MyAwesomeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using MyAwesomeApp.ViewModels;
+using System;
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,18 +10,11 @@ namespace MyAwesomeApp.Views
     public partial class CoffeeEquipmentPage : ContentPage
     {
 
-        int count = 0;
-
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
 
-            LabelCount.Text = "Hello from code behind";
-        }
-
-        private void ButtonClick_Clicked(object sender, EventArgs e)
-        {
-            LabelCount.Text = $"You clicked {++count} times";
+            //BindingContext = new CoffeeEquipmentViewModel();
         }
     }
 }
